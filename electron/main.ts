20d31999a47c44cd4ddef602325e0932395f46ea
JupiterPi <hello@jupiterpi.de>
2025-04-19 02:19:13 +0200

    feat: basic tray icon

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain } from "electron"
+import { app, BrowserWindow, ipcMain, Menu, nativeImage, Tray } from "electron"
 import { fileURLToPath } from "node:url"
 import path from "node:path"
 
@@ -12,6 +12,7 @@ export const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist")
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST
 
 let window: BrowserWindow | null
+let tray: Tray | null = null
 
 function createWindow() {
   window = new BrowserWindow({
@@ -25,14 +26,12 @@ function createWindow() {
   } else {
     window.loadFile(path.join(RENDERER_DIST, "index.html"))
   }
-}
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit()
-    window = null
-  }
-})
+  window.on("close", (event) => {
+    event.preventDefault()
+    window?.hide()
+  })
+}
 
 app.on("activate", () => {
   // for macOS
@@ -41,9 +40,44 @@ app.on("activate", () => {
   }
 })
 
+let isActive = false
+
+const activeTrayIcon = nativeImage.createFromPath(path.join(process.env.VITE_PUBLIC, "tray_icon_active.png"))
+const inactiveTrayIcon = nativeImage.createFromPath(path.join(process.env.VITE_PUBLIC, "tray_icon_inactive.png"))
+
+function setActive() {
+  isActive = true
+  tray?.setToolTip("Thyme (active)")
+  tray?.setImage(activeTrayIcon)
+}
+
+function setInactive() {
+  isActive = false
+  tray?.setToolTip("Thyme (inactive)")
+  tray?.setImage(inactiveTrayIcon)
+}
+
 app.whenReady().then(() => {
   createWindow()
   ipcMain.handle("test", ipcHandler.test)
+
+  // tray icon
+  tray = new Tray(inactiveTrayIcon)
+  tray.setTitle("Thyme")
+  setInactive()
+  let toggleActiveTimer: NodeJS.Timeout | null = null
+  tray.on("click", () => {
+    toggleActiveTimer = setTimeout(() => isActive ? setInactive() : setActive(), 200)
+  })
+  tray.on("double-click", () => {
+    if (toggleActiveTimer) clearTimeout(toggleActiveTimer)
+    window?.isVisible() ? window?.hide() : window?.show()
+  })
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Open Thyme", type: "normal", click: () => window ? window.show() : createWindow() },
+    { label: "Toggle", type: "normal", click: () => isActive ? setInactive() : setActive() },
+  ])
+  tray.setContextMenu(contextMenu)
 })
 
 const ipcHandler: IPC = {
